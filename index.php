f11e973442c6f879c1816c4bc2725e312b18df04
Glaubinho03 <85969179+juh22jjuh@users.noreply.github.com>
2022-10-04 11:42:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Adição</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Adição</h1>
+        <form action="adiçao.php" method="POST">
+          Insira o primeiro número: <input type="text" name="n1">      
+          <br>
+          Insira o segundo número: <input type="text" name="n2" >
+          <br>
+          <input type="submit"  value="Enviar">
+          <input type="reset"   value="Cancelar">
+        </form>
+</body>
+</html>
